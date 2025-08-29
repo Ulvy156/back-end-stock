e2beee8f08abec005335191ff48c0d699bf4bf18
Ulvy Romy <ulvyromy156@gmail.com>
2025-08-29 21:41:24 +0700

    feat(cors): add CORS configuration and enable CORS in the application

---
+++ src/config/cors.config.ts
@@ -0,0 +1,7 @@
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
+
+export const corsConfig: CorsOptions = {
+  origin: [process.env.FRONT_END_URL ?? ''],
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true,
+};
--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { JwtAuthGuard } from './auth/jwt-auth.gurad';
+import { corsConfig } from './config/cors.config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -9,9 +10,15 @@ async function bootstrap() {
   const reflector = app.get(Reflector);
   // Apply JWT guard globally
   app.useGlobalGuards(new JwtAuthGuard(reflector));
+
+  // enable validate
   app.useGlobalPipes(
     new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }),
   );
+
+  //enable cors
+  app.enableCors(corsConfig);
+
   await app.listen(process.env.PORT ?? 3000);
 }
-bootstrap();
+void bootstrap();
