4546a31f2a90c8a83d3019b4138813e17cd7310e
Ulvy Romy <ulvyromy156@gmail.com>
2025-09-02 09:47:24 +0700

    register warehouse module

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,9 +6,17 @@ import { UserModule } from './user/user.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './auth/auth.module';
 import { CustomersModule } from './customers/customers.module';
+import { WarehouseModule } from './warehouse/warehouse.module';
 
 @Module({
-  imports: [ConfigModule, UserModule, PrismaModule, AuthModule, CustomersModule],
+  imports: [
+    ConfigModule,
+    UserModule,
+    PrismaModule,
+    AuthModule,
+    CustomersModule,
+    WarehouseModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
